e6af1eabd7ee5319038e1ce9b82275999c1cdbd8
Muruts Gebretsadik <murutsmenge21@Murutss-MacBook-Air.local>
2024-08-02 17:40:00 +0100

    Added final-project-patrick-garage.onrender.com to ALLOWED_HOSTS

--- patrick_garage/settings.py
+++ patrick_garage/settings.py
@@ -32,6 +32,7 @@ DEBUG = os.environ.get("DEBUG", "False").lower() == 'true'
 
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
+ALLOWED_HOSTS = ['final-project-patrick-garage.onrender.com']
 
 
 
