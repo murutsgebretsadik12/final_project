4c4c15fb2ae980b54340c03772623faafa1cacbc
Muruts Gebretsadik <murutsmenge21@Murutss-MacBook-Air.local>
2024-08-04 08:41:41 +0100

    update the setting

--- patrick_garage/settings.py
+++ patrick_garage/settings.py
@@ -100,8 +100,8 @@ DATABASES = {
     }
 }
 
-# database_url = os.environ.get("DATABASE_URL")
-# DATABASES["default"]= dj_database_url.parse(database_url)
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"]= dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
