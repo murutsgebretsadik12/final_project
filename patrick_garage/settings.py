7f8ca302cf9508d127a5b80b0f7e16abd90ec12a
Muruts Gebretsadik <murutsmenge21@Murutss-MacBook-Air.local>
2024-08-11 07:57:47 +0100

    update the contact.html

--- patrick_garage/settings.py
+++ patrick_garage/settings.py
@@ -18,6 +18,7 @@ import dj_database_url
 from pathlib import Path
 
 
+GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,12 +34,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False").lower() == 'true'
 
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
-ALLOWED_HOSTS = ['final-project-patrick-garage.onrender.com']
-ALLOWED_HOSTS = ['final-project-patrick-garage.onrender.com', 'localhost 127.0.0.1 [::1]']
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
+# ALLOWED_HOSTS = ['final-project-patrick-garage.onrender.com']
+# ALLOWED_HOSTS = ['final-project-patrick-garage.onrender.com', 'localhost 127.0.0.1 [::1]']
 
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
@@ -136,7 +137,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
+
 
 
 # Static files (CSS, JavaScript, Images)
