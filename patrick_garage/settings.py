ed164c9e8a4df1d2f7aea19eb4779a57df61564d
Muruts Gebretsadik <murutsmenge21@Murutss-MacBook-Air.local>
2024-08-11 08:56:18 +0100

    update the setting

--- patrick_garage/settings.py
+++ patrick_garage/settings.py
@@ -18,7 +18,8 @@ import dj_database_url
 from pathlib import Path
 
 
-GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
+# GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
+GOOGLE_MAPS_API_KEY = os.environ.get("GOOGLE_MAPS_API_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
